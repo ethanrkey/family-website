1120a9b623ac89ce4baf5f251f672b6c2238ef7e
Ethan Key <ethanrkey23@gmail.com>
2025-01-16 16:36:30 -0600

    updating home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,51 +7,59 @@ import DynamicTitle from "@/components/browsertitle";
 export default function Home() {
   return (
     
-    <div className="bg-gray-800 text-white min-h-screen">
+    <div className="bg-[#1F2937] text-white min-h-screen">
       <DynamicTitle title="Key Family"/>
       {/* Hero Section */}
-        <h1 className="text-8xl leading-tight text-primary text-center py-6 font-heading border border-green-500">Key Family</h1>
-        <p className="text-4xl leading-tight text-primary text-center py-6 border border-green-500">Dedicated. Resilient. Determined.</p>
-        
-      {/* Family Members Section */}
-      <div className="flex flex-row items-center justify-between px-6 md:px-16 py-5 gap-7 my-6 border border-green-500">
-        {/* Amy Key */}
-        <div className="flex flex-col justify-center items-center border border-yellow-500">
-          <Avatar alt="Amy Key" src="/mom3.jpg" sx={{ width: 150, height: 150 }}/>
-          <h2 className="text-2xl font-semibold">Amy Key</h2>
+        <h1 className="text-8xl leading-tight text-primary text-center py-6 font-heading border-b-2 border-[#ffffff]">Key Family</h1>
+        <div className="bg-[#687a94]">
+          <p className="text-4xl leading-tight text-primary text-center pt-16 border border-green-500">Dedicated. Resilient. Determined.</p>
+          
+        {/* Family Members Section */}
+        <div className="flex flex-row items-center justify-between px-16 py-10 my-20 border-b border-t border-[#ffffff]">
+          {/* Amy Key */}
+          <div className="flex flex-col w-full justify-center items-center p-8 border border-yellow-500">
+            <Avatar alt="Amy Key" src="/mom3.jpg" className="scale-150" sx={{ width: 150, height: 150 }}/>
+            <h2 className="text-3xl font-semibold pt-14">Amy Key</h2>
+          </div>
+          <div className="ml-16 px-44 border border-yellow-500">
+            <p className="text-center">
+              Amy is a Registered Nurse, Nurse Injector, Medical Claim Assessor, and Yoga Instructor. She graduated from the UT Arlington College of Nursing in 19__, earning a Bachelor of Science in Nursing. Amy is a mother to two sons, Ethan and Bryce, and two dogs, Gigi and Geoffrey. She enjoys spending time with her sons, her dogs, and her best friends, &quot;the Beavers&quot;. Amy is a lifelong Texan, born and raised in DFW.
+            </p>
+          </div>
         </div>
-        <div className="border border-yellow-500">
-          <p>
-            Amy is a Registered Nurse, Nurse Injector, Medical Claim Assessor, and Yoga Instructor. She graduated from the UT Arlington College of Nursing in 19__, earning a Bachelor of Science in Nursing. Amy is a mother to two sons, Ethan and Bryce, and two dogs, Gigi and Geoffrey. She enjoys spending time with her sons, her dogs, and her best friends, &quot;the Beavers&quot;. Amy is a lifelong Texan, born and raised in DFW.
-          </p>
-        </div>
-      </div>
 
-      <div className="flex flex-row items-center justify-between px-6 md:px-16 py-5 gap-7 my-6 border border-green-500">
-        {/* Ethan Key */}
-        <div className="border border-yellow-500">
-          <p>
-          imetay isyay ayay undamentalfay aspectyay ofyay ouryay experienceyay ofyay ethay orldway , utbay itsyay alsoyay oneyay ofyay ethay ostmay eriousmystay andyay ebatedday enomenaphay inyay othbay iencescay andyay ilosophyphay . onyay ayay asicbay evellay , imetay eemssay straightforward—its atwhay eway easuremay ithway ocksclay , ethay easonray eway avehay ayday andyay ightnay , ethay ickingtay ofyay econdssay , inutesmay , andyay ourshay . utbay enwhay ouyay artstay otay igday eeperday , ouyay ealizeray ustjay owhay omplexcay andyay angestray imetay eallyray isyay . in icsphysay , imetay isyay oftenyay eatedtray asyay ayay imensionday .
-          </p>
+        <div className="flex flex-row items-center justify-between px-16 py-10 my-20 border-b border-t border-[#ffffff]">
+          {/* Ethan Key */}
+          <div className="mr-16 px-44 border border-yellow-500">
+            <p className="text-center">
+              Ethan is in his junior year at Boston University. He studies computer science, and aspires to be a software engineer. Ethan has been on the Dean&apos;s List in the 2024 Spring and Fall semesters, maintaining a cumulative GPA of 3.67. Ethan enjoys spending time with his family and friends, setting goals, working out, biking, listening to music, looking at the stock market, and watching various sports.
+            </p>
+          </div>
+          <div className="flex flex-col w-full justify-center items-center p-8 border border-yellow-500">
+            <Link href="/ethankey">
+                <Avatar alt="Ethan Key" src="/me.jpg" className="scale-150" sx={{ width: 150, height: 150,
+                  transition: "all 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+                  "&:hover": {
+                    transform: "scale(1.6)", // Slightly enlarge on hover
+                    boxShadow: "0px 4px 10px rgba(255, 255, 255, 0.5)", // Glow effect
+                  },
+                 }}/>
+            </Link>
+            <h2 className="text-3xl font-semibold pt-14">Ethan Key</h2>
+          </div>
         </div>
-        <div className="flex flex-col justify-center items-center border border-yellow-500">
-          <Link href="/ethankey">
-              <Avatar alt="Ethan Key" src="/me.jpg" sx={{ width: 150, height: 150 }}/>
-          </Link>
-          <h2 className="text-2xl font-semibold">Ethan Key</h2>
-        </div>
-      </div>
 
-      <div className="flex flex-row items-center justify-between px-6 md:px-16 py-5 gap-7 my-6 border border-green-500">
-        {/* Bryce Key */}
-        <div className="flex flex-col justify-center items-center border border-yellow-500">
-          <Avatar alt="Bryce Key" src="/bryce2.jpg" sx={{ width: 150, height: 150 }}/>
-          <h2 className="text-2xl font-semibold">Bryce Key</h2>
-        </div>
-        <div className="border border-yellow-500">
-          <p>
-            Bryce is currently a freshman at Columbia University, where he studies Biochemistry and Philosophy and competes on their Swimming and Diving team. Bryce graduated his high school, The Oakridge School, as the Valedictorian of his class. He is an outstanding scholar, earning a 4.96 weighted GPA in high school, scoring a 35 on the ACT, and currently maintaining a 4.0 GPA at Columbia. In his free time, Bryce likes to spend time with friends, read, crochet, sing, and design fashion. 
-          </p>
+        <div className="flex flex-row items-center justify-between px-16 py-10 mt-20 border-b border-t border-[#ffffff]">
+          {/* Bryce Key */}
+          <div className="flex flex-col w-full justify-center items-center p-8 border border-yellow-500">
+            <Avatar alt="Bryce Key" src="/bryce2.jpg" className="scale-150" sx={{ width: 150, height: 150 }}/>
+            <h2 className="text-3xl font-semibold pt-14">Bryce Key</h2>
+          </div>
+          <div className="ml-16 px-44 border border-yellow-500">
+            <p className="text-center">
+              Bryce is currently a freshman at Columbia University, where he studies Biochemistry and Philosophy and competes on their Swimming and Diving team. Bryce graduated his high school, The Oakridge School, as the Valedictorian of his class. He is an outstanding scholar, earning a 4.96 weighted GPA in high school, scoring a 35 on the ACT, and currently maintaining a 4.0 GPA at Columbia. In his free time, Bryce likes to spend time with friends, read, crochet, sing, and design fashion. 
+            </p>
+          </div>
         </div>
       </div>
 
